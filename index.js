cd54f8dc2d1f2668444501a442d61173d9a8a9d4
Maxun <trukhinmaksimof@mail.ru>
2022-09-22 20:33:17 +0800

    starting to realize submit button

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-function submitPasswordForm(form) {
+function generateRandomSymbol(args) {
+    return 0;
+}
+
+function generatePassword({ passwordRangeSettings, defaultGenerate} = { passwordRangeSettings : undefined, defaultGenerate : true}) {
+    const outputPassword = [];
+    
+    if (defaultGenerate) {
+        for (let i = 0; i < passwordLength; i++) {
+            return 0;
+        }
+    }
+    
+}
+
+// ***
+
+function submitForm(btn) {
+    const form = btn.form;
+    
+    console.log(btn.name);
+    console.log(btn.form);
+    if (btn.name === "password-number-symbols-setttings") {
+        generatePassword({ passwordRangeSettings : passwordRangeSettings, defaultGenerate : false});
+    } else {
+        generatePassword({ passwordRangeSettings : undefined, defaultGenerate : false});
+    }
     return 0;
 };
 
 const PASSWORD_MIN_LEN = 4;
 let passwordLength = 4;
 let passwordSymbolDivHTML = $("#password-symbols-settings").clone().html();
-let passwordNumbersSetting = { names : {"number-range-let" : 0, "number-range-num" : 0, "number-range-sym" : 0}, sum : 0};
+let passwordRangeSettings = { names : {"number-range-let" : 0, "number-range-num" : 0, "number-range-sym" : 0}, sum : 0};
+const passwordSymbolsSetting = [];
+let lastActiveField = undefined;
 
 function inputSum(name = "number-range-let") {
     let sum = 0;
-    for (let i in passwordNumbersSetting.names) {
-        sum += +passwordNumbersSetting.names[i];
+    for (let i in passwordRangeSettings.names) {
+        sum += +passwordRangeSettings.names[i];
     }
-    passwordNumbersSetting.sum = sum;
+    passwordRangeSettings.sum = sum;
     return sum;
 }
 
@@ -22,8 +50,8 @@ function setInputsValues() {
     let rangeValue = 0;
     let $range = null;
 
-    for (let name in passwordNumbersSetting.names) {
-        rangeValue = passwordNumbersSetting.names[name];
+    for (let name in passwordRangeSettings.names) {
+        rangeValue = passwordRangeSettings.names[name];
         $fields.find(`input[name=${name}]`).val(rangeValue);
         $range = $($fields.find(`input[name=${name}][type=range]`));
         $range[0] && $range.attr("class", $range.attr("class").replace(/slider-fill-\d+/, sliderClassName + (0 | ((rangeValue) * 100 / (passwordLength)))));
@@ -35,37 +63,23 @@ function sliderMove(_this) {
     let $thisVal = $this.val();
     const sliderClassName = "slider-fill-";
     let sliderElem = null;
-    // error here \/
     
-    //passwordNumbersSetting.sum += $thisVal - passwordNumbersSetting[_this.name];
-    passwordNumbersSetting.names[_this.name] = +$thisVal;
+    //lastActiveField = "#password-number-symbols-setttings";
+    passwordRangeSettings.names[_this.name] = +$thisVal;
     inputSum();
-    //inputSum();
     
-    if (passwordNumbersSetting.sum > passwordLength) {
-        for (let name in passwordNumbersSetting.names) {
+    if (passwordRangeSettings.sum > passwordLength) {
+        for (let name in passwordRangeSettings.names) {
             if (_this.name !== name) {
-                passwordNumbersSetting.names[name] -= Math.min((passwordNumbersSetting.sum - passwordLength), passwordNumbersSetting.names[name]);
+                passwordRangeSettings.names[name] -= Math.min((passwordRangeSettings.sum - passwordLength), passwordRangeSettings.names[name]);
                 if (inputSum() === passwordLength) {break;}
             }
         }
     }
-    console.table(passwordNumbersSetting.names);
-    console.log("Summa:", passwordNumbersSetting.sum);
+    console.table(passwordRangeSettings.names);
+    console.log("Summa:", passwordRangeSettings.sum);
     
     setInputsValues();
-    //
-    /*
-    $($this.siblings(`input[name=${_this.name}]`)).val($thisVal);
-    if ($this.attr("type") === "range") {
-        sliderElem = $this;
-    } else {
-        sliderElem = $($this.siblings(`input[name=${_this.name}]`));
-    }
-    
-    sliderElem.attr("class", sliderElem.attr("class").replace(/slider-fill-\d+/, sliderClassName + (0 | (($thisVal) * 100 / (sliderElem.attr("max"))))));
-    console.log(sliderElem.attr("class"));
-    */
 };
 
 function changePasswordLength(_this) {
@@ -84,17 +98,14 @@ function changePasswordLength(_this) {
     clonePasswordSymbolsHTMLElements(passwordLength);
 };
 
-
 function clonePasswordSymbolsHTMLElements(passwordLength = PASSWORD_MIN_LEN) {
     const symbolsNumber = $("#password-symbols-settings").children().length;
     
-    //console.log("To remove: ", symbolsNumber - passwordLength);
     for (let _ = 0; _ < symbolsNumber - passwordLength; _++) {
-        //console.log(passwordSymbolDivHTML);
         $("#password-symbols-settings > *:first-child").remove();
+        //passwordSymbolsSetting.length = symbolsNumber;
     }
     for (let _ = 0; _ < passwordLength - symbolsNumber; _++) {
-        //console.log(passwordSymbolDivHTML);
         $("#password-symbols-settings").append(passwordSymbolDivHTML);
     }
 }
@@ -102,7 +113,7 @@ function clonePasswordSymbolsHTMLElements(passwordLength = PASSWORD_MIN_LEN) {
 $(document).ready(() => {
     $("button[type=submit]").click(function (e) {
         e.preventDefault();
-        submitPasswordForm(this.form);
+        submitForm(this);
     });
     $("button[title='Settings']").click(function (e) {
         e.preventDefault();
---
+++ rtest.js
@@ -0,0 +1,4 @@
+const f = ({a, b} = {a : 9, b : 10}) => {
+    console.log(a, b);
+}
+f();
